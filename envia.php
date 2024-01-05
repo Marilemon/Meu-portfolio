d6b5f7e859c65070949274b0cefdeab96de87e64
Mariana Muniz <mmuniz2209@gmail.com>
2024-01-05 16:08:20 -0300

    Corrigindo php

--- envia.php
+++ envia.php
@@ -2,18 +2,18 @@
 $nome = addslashes($_POST['nome']);
 $email = addslashes($_POST['email']);
 $telefone = addslashes($_POST['telefone']);
+$mensagem = addslashes($_POST['mensagem']); // Adicionei esta linha
 
 $para = "mmuniz2209@gmail.com";
-$assunto = "Fale comigo - portifolio";
+$assunto = "Fale comigo - portfólio";
 
-$corpo = "Nome:  ".$nome."\n"."Email:  ".$email."\n"."Telefone:  ".$telefone."\n".
+$corpo = "Nome:  " . $nome . "\n" . "Email:  " . $email . "\n" . "Telefone:  " . $telefone . "\n" . "Mensagem: " . $mensagem . "\n"; // Corrigi esta linha
 
-$cabeca = "From: limao.oliveira1809@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+$cabeca = "From: limao.oliveira1809@gmail.com" . "\n" . "Reply-to: " . $email . "\n" . "X-Mailer: PHP/" . phpversion(); // Corrigi esta linha
 
-if(mail ($para,$assunto,$corpo,$cabeca)){
+if(mail($para, $assunto, $corpo, $cabeca)) {
     echo("E-mail enviado com sucesso!");
-}else{
-    echo("houve um erro ao enviar o e-mail :(");
+} else {
+    echo("Houve um erro ao enviar o e-mail :(");
 }
-
 ?>
